272da3e335808fda4d1d397265adb5616f9cb042
Christopher Holzmann Pérez <christopherholzmannperez@gmail.com>
2024-02-04 16:40:59 +0100

    Add shared AssemblyInfo for CLI and GUI projects

---
+++ RyuAssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// Shared assembly info for both SRMM projects
+using System.Reflection;
+
+[assembly: AssemblyTitle("Yakuza/Like a Dragon Mod Manager")]
+[assembly: AssemblyCopyright("Copyright © 2024 Shin Ryu Mod Manager Studio")]
+
+[assembly: AssemblyVersion("4.0.3")]
---
+++ RyuGUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyProduct("Shin Ryu Mod Manager GUI")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a0824575-886a-4504-9ee9-e5b196b9be5e")]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
