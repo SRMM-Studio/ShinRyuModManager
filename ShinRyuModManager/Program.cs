9a3e269f78edd28c7c9e24fe532a0e5d8e96e149
Jhrino <orhankaymak@windowslive.com>
2026-06-23 02:24:52 +0300

    Disable updater for now

--- ShinRyuModManager/Program.cs
+++ ShinRyuModManager/Program.cs
@@ -188,7 +188,9 @@ public static class Program
             
             if (_iniData.TryGetKey("RyuModManager.CheckForUpdates", out var check))
             {
-                CheckForUpdates = int.Parse(check) == 1;
+#warning TODO 23.06.2026: Reimplement updates for Github once we're good to go on NexusMods again
+                CheckForUpdates = false;
+                //CheckForUpdates = int.Parse(check) == 1;
             }
             
             if (_iniData.TryGetKey("RyuModManager.ShowWarnings", out var showWarnings))
